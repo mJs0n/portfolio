6fe8778194998ed920efb9d47e40b3ab48020e5f
mJs0n <mjzone15@gmail.com>
2026-02-27 15:07:44 +0800

    V2.4

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/HomeS";
+import Home from "./pages/Home";
 import ProjectDetails from "./pages/ProjectDetails";
 
 function App() {
